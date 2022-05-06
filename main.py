6a88e1dd271b0fb61e3accdbbb8fd223728c4de0
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2022-05-06 13:20:38 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from viajero import Viajero
+if __name__=='__main__':
+    lista = []
+    v=Viajero(321, 721323,'Marcos', 'Maroni', 93032)
+    lista.append(viajero)
+    v1=Viajero(214, 821821, 'Horacio', 'Dartes', 81282)
+    lista.append(viajero1)
+    lista.mostrar()
+    lista.sort()
+    for l in lista:
+      l.mostrar()
+      
+    v = v + 300
+    v.mostrar()
+    if v1 > 500:
+    v1= v1 - 2000
+    v1.mostrar()
