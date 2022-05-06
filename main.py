6c6287a95acb267f65d840baa16ffbaf45713f21
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2022-05-06 14:59:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from viajero import Viajero
 if __name__=='__main__':
-    lista = []
     v=Viajero(321, 721323,'Marcos', 'Maroni', 93032)
-    lista.append(viajero)
     v1=Viajero(214, 821821, 'Horacio', 'Dartes', 81282)
-    lista.append(viajero1)
-    lista.mostrar()
-    lista.sort()
-    for l in lista:
-      l.mostrar()
-      
+    if v > v1:
+        print('el viajero con mas millas es: Marcos')
+     else: print('el viajero con mas millas es: Horacio')
+    
+    print('el viajero con mas millas es (1=v|2=v1): {}'.format(self.s))
     v = v + 300
     v.mostrar()
     if v1 > 500:
